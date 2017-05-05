2b51ca3bd54965c01d4339aa3a8c2d2056a6a143
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-05-05 09:58:19 -0300

    Added primary key (See #5) and update test

--- tests/ci/travis/download.js
+++ tests/ci/travis/download.js
@@ -1,7 +1,11 @@
 var system = require('system');
 var env = system.env;
 
-function loginIBM() {
+function loginIBM(status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+        phantom.exit(1);
+    }
     system.stdout.writeLine('Logging');
 
     page.onLoadFinished = downloadPage;
@@ -9,8 +13,11 @@ function loginIBM() {
         document.getElementById('firstName').value = 'test';
         document.getElementById('lastName').value = 'travis';
         document.getElementById('emailAddress').value = 'travis@test.tst';
+        document.getElementById('phone').value = '0000-0000';
         document.getElementById('company').value = 'test for travis';
         document.getElementById('countryResidence').value = 'US';
+        document.getElementById('state').innerHTML = '<option value="AK">Alaska</option>';
+        document.getElementById('state').value = 'AK';
 
         document.getElementById('NC_CHECK_EMAIL').checked = false;
         document.getElementById('NC_CHECK_PHONE').checked = false;
@@ -22,18 +29,34 @@ function loginIBM() {
     });
 }
 
-function downloadPage() {
+function downloadPage(status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+        phantom.exit(1);
+    }
+
     system.stdout.writeLine('Going to download http page');
     page.onLoadFinished = downloadLink;
-//    page.evaluate(function () {
-//        var tab = document.getElementsByClassName('ibm-last-tab')[0];
-//        tab.childNodes[0].click();
-//    });
-    page.open('https://www-01.ibm.com/marketing/iwm/iwm/web/download.do?source=swg-db2expressc&S_PKG=dllinux64&S_TACT=100KG28W&lang=en_US&&&dlmethod=http');
+    var r = page.evaluate(function () {
+        var link = document.querySelector('.ibm-last-tab a');
+        if (link.getAttribute('href') === '#') {
+            return 'ok';
+        } else {
+            link.click();
+        }
+    });
 
+    if (r === 'ok') {
+        downloadLink('success');
+    }
 }
 
-function downloadLink() {
+function downloadLink(status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+        phantom.exit(1);
+    }
+
     system.stdout.writeLine('Picking download link');
     var link = page.evaluate(function () {
         return document.getElementsByClassName('ibm-download-link')[0].href;
@@ -54,9 +77,15 @@ system.stdout.writeLine('Starting');
 var page = require('webpage').create();
 page.settings.userAgent = 'Mozilla/5.0';
 
+page.onConsoleMessage = function (msg) {
+    system.stderr.writeLine('CONSOLE: ' + msg);
+};
+
 page.onResourceError = function (error) {
 //    system.stderr.writeLine(JSON.stringify(error));
+    system.stderr.writeLine(error.url + ': ' + error.errorString);
 //    phantom.exit(1);
 }
+
 page.onLoadFinished = loginIBM;
-page.open('https://www-01.ibm.com/marketing/iwm/iwm/web/pick.do?source=swg-db2expressc&S_PKG=dllinux64&S_TACT=100KG28W&lang=en_US&dlmethod=http');
+page.open('https://www-01.ibm.com/marketing/iwm/iwm/web/signup.do?source=swg-db2expressc&S_TACT=100KG28W&lang=en_US&dlmethod=http&S_PKG=ov48105');
